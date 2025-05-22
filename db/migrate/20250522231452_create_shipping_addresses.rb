341cf482aaba8cd4fd55c143ae4747ffd44a2225
Ken8712 <skyblow.9@gmail.com>
2025-05-23 08:30:11 +0900

    マイグレーションの実行

--- db/migrate/20250522231452_create_shipping_addresses.rb
+++ db/migrate/20250522231452_create_shipping_addresses.rb
@@ -4,7 +4,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[7.1]
       t.string :postal_code, null: false
       t.string :city, null: false
       t.string :street_address, null: false
-      t.string :building_name,
+      t.string :building_name
       t.string :phone_number, null: false
       t.integer :prefecture_id, null: false
       t.references :order, null: false, foreign_key: true

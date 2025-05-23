9b405a9f6fc04c4e7ac9868461578d9dc787a37b
Ken8712 <skyblow.9@gmail.com>
2025-05-23 13:52:58 +0900

    商品購入操作により、注文情報と配送先住所を保存する機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      binding.pry
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_shipping_address).permit(:postal_code,:prefecture_id,:city,:street_address,:building_name,:phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -5,7 +5,7 @@ class OrderShippingAddress
   with_options presence: true do
     validates :user_id, :item_id, :city, :street_address
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "must be 10 or 11 digits and not include hyphens."}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "must be 10 or 11 digits and not include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   end
 

c529df8a85ee08b6995aac3c6701932e6224441b
Ken8712 <skyblow.9@gmail.com>
2025-05-23 18:35:12 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
   end
@@ -10,17 +11,27 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
+      pay_item
       @order_shipping_address.save
-      binding.pry
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
 
   private
   def order_params
-    params.require(:order_shipping_address).permit(:postal_code,:prefecture_id,:city,:street_address,:building_name,:phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_shipping_address).permit(:postal_code,:prefecture_id,:city,:street_address,:building_name,:phone_number).merge(user_id: current_user.id, item_id: @item.id,token: params[:token],price: @item.price)
+  end
+
+  def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: params[:token],
+        currency: 'jpy'
+      )
   end
 
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,9 +1,9 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :token, :price
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :street_address
+    validates :user_id, :item_id, :city, :street_address, :token, :price
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "must be 10 or 11 digits and not include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
